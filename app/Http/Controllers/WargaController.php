4ac778d693b9471494801fe70939d50d62449bc1
andi <echo.matuladaandi@gmail.com>
2022-12-05 21:32:21 +0800

    read data warga

--- app/Http/Controllers/WargaController.php
+++ app/Http/Controllers/WargaController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Warga;
 use Illuminate\Http\Request;
 
 class WargaController extends Controller
 {
     public function index()
     {
-        return view('warga.index');
+        // $warga harus sama dgn compact warga, compact = artinya lemparkan data  
+        $warga = Warga::all();
+        return view('warga.index', compact(['warga']));
     }
 }
--- resources/views/warga/index.blade.php
+++ resources/views/warga/index.blade.php
@@ -1,5 +1,33 @@
-<html>
-    <body>
-        <h1>Tes</h1>
-    </body>
-</html>
+<table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">No</th>
+        <th scope="col">NIK</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Tempat,Tgl Lahir</th>
+        <th scope="col">Jenis Kelamin</th>
+        <th scope="col">Alamat</th>
+        <th scope="col">Agama</th>
+        <th scope="col">Status Perkawinan</th>
+        <th scope="col">Pekerjaan</th>
+        <th scope="col">Kewarganegaraan</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($warga as $item)
+        <tr>
+            {{-- <td>{{ $item-> }}</td> --}}
+            <td>1</td>
+            <td>{{ $item->nik }}</td>
+            <td>{{ $item->nama }}</td>
+            <td>{{ $item->ttl }}</td>
+            <td>{{ $item->j_klmn }}</td>
+            <td>{{ $item->alamat }}</td>
+            <td>{{ $item->agama }}</td>
+            <td>{{ $item->sts_perkawinan }}</td>
+            <td>{{ $item->pekerjaan }}</td>
+            <td>{{ $item->warganegara }}</td>
+          </tr>
+        @endforeach
+    </tbody>
+  </table>
