e8e435fe7ead78903812d1ab4820961f4e45209b
andi <echo.matuladaandi@gmail.com>
2022-12-05 22:44:38 +0800

    perbaikan form create

--- resources/views/warga/create.blade.php
+++ resources/views/warga/create.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
     <div class="col-lg-7">
-        <form>
+        <form action="/store" method="POST">
             @csrf
             <div class="mb-3">
             <label for="nik" class="form-label">NIK</label>
@@ -21,8 +21,11 @@
             </div>
 
             <div class="mb-3">
-            <label for="j_klm" class="form-label">Jenis Kelamin</label>
-            <input type="text" class="form-control" id="j_klm" name="j_klm">
+                <select name="j_klmn" id="j_klmn">
+                    <option value="">Pilih Jenis Kelamin</option>
+                    <option value="L">Laki-Laki</option>
+                    <option value="P">Perempuan</option>
+                </select>
             </div>
 
             <div class="mb-3">
@@ -36,20 +39,25 @@
             </div>
 
             <div class="mb-3">
-            <label for="sts_perkawinan" class="form-label">Status Perkawinan</label>
-            <input type="text" class="form-control" id="sts_perkawinan" name="sts_perkawinan">
+                <select name="sts_perkawinan" id="sts_perkawinan">
+                    <option value="">Status Perkawinan</option>
+                    <option value="S">Sudah Kawin</option>
+                    <option value="B">Belum Kawin</option>
+                </select>
             </div>
 
             <div class="mb-3">
             <label for="pekerjaam" class="form-label">Pekerjaan</label>
             <input type="text" class="form-control" id="pekerjaam" name="pekerjaam">
             </div>
-
+            
+            <div class="mb-3">
             <label for="warganegara" class="form-label">Kewarganegaraan</label>
             <input type="text" class="form-control" id="warganegara" name="warganegara">
-
             </div>
+
             <button type="submit" class="btn btn-primary">Simpan</button>
+            <a href="/" class="btn btn-warning">Batal</a>
         </form>
     </div>
 
