c8aa68fe3531f7c138d55e00af46d0dd126a22be
andi <echo.matuladaandi@gmail.com>
2022-12-10 11:20:59 +0800

    view login,logout,middleware dan logicnya selesai

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', [WargaController::class, 'index']);
 Route::get('/wargaCreate', [WargaController::class, 'create']);
 Route::post('/wargaStore', [WargaController::class, 'store']);
