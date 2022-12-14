abab3a98f334e916ced9252d4f2c75c5be459123
andi <echo.matuladaandi@gmail.com>
2022-12-14 22:40:12 +0800

    menambahkan requred pd input di file create

--- resources/views/warga/create.blade.php
+++ resources/views/warga/create.blade.php
@@ -7,21 +7,21 @@
             @csrf
             <div class="mb-3">
             <label for="nik" class="form-label">NIK</label>
-            <input type="text" class="form-control" id="nik" name="nik">
+            <input type="text" class="form-control" id="nik" name="nik" required>
             </div>
 
             <div class="mb-3">
             <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama" name="nama">
+            <input type="text" class="form-control" id="nama" name="nama"  required>
             </div>
 
             <div class="mb-3">
             <label for="ttl" class="form-label">Tempat, Tgl Lahir</label>
-            <input type="text" class="form-control" id="ttl" name="ttl">
+            <input type="text" class="form-control" id="ttl" name="ttl"  required>
             </div>
 
             <div class="mb-3">
-                <select name="j_klmn" id="j_klmn">
+                <select name="j_klmn" id="j_klmn"  required>
                     <option value="">Pilih Jenis Kelamin</option>
                     <option value="L">Laki-Laki</option>
                     <option value="P">Perempuan</option>
@@ -30,16 +30,16 @@
 
             <div class="mb-3">
             <label for="alamat" class="form-label">Alamat</label>
-            <input type="text" class="form-control" id="alamat" name="alamat">
+            <input type="text" class="form-control" id="alamat" name="alamat"  required>
             </div>
 
             <div class="mb-3">
             <label for="agama" class="form-label">Agama</label>
-            <input type="text" class="form-control" id="agama" name="agama">
+            <input type="text" class="form-control" id="agama" name="agama"  required>
             </div>
 
             <div class="mb-3">
-                <select name="sts_perkawinan" id="sts_perkawinan">
+                <select name="sts_perkawinan" id="sts_perkawinan" required>
                     <option value="">Status Perkawinan</option>
                     <option value="S">Sudah Kawin</option>
                     <option value="B">Belum Kawin</option>
@@ -48,12 +48,12 @@
 
             <div class="mb-3">
             <label for="pekerjaan" class="form-label">Pekerjaan</label>
-            <input type="text" class="form-control" id="pekerjaan" name="pekerjaan">
+            <input type="text" class="form-control" id="pekerjaan" name="pekerjaan" required>
             </div>
             
             <div class="mb-3">
             <label for="warganegara" class="form-label">Kewarganegaraan</label>
-            <input type="text" class="form-control" id="warganegara" name="warganegara">
+            <input type="text" class="form-control" id="warganegara" name="warganegara" required>
             </div>
 
             <button type="submit" class="btn btn-primary">Simpan</button>
